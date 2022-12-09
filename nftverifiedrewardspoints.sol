47eee2f8d3eb2a346bfdbc14e6228e1c37367610
MyBlokchainGuru <119838455+MyBlokchainGuru@users.noreply.github.com>
2022-12-09 01:33:31 -0600

    Update nftverifiedrewardspoints.sol
    Update smart contract for a rewards points token A that can has an airdrop to one address function. Create a function where if a wallet redeems 10000 token A, they are airdropped an nft A held in the contract.

--- nftverifiedrewardspoints.sol
+++ nftverifiedrewardspoints.sol
@@ -1,71 +1,74 @@
-pragma solidity ^0.6.0;
+pragma solidity ^0.5.12;
 
-contract RewardsToken {
-    // The token name
-    string public name = "TokenA";
+contract RewardsTokenA {
 
-    // The token symbol
-    string public symbol = "TA";
-
-    // The total supply of tokens
+    string public name;
+    string public symbol;
+    uint8 public decimals;
     uint256 public totalSupply;
-
-    // The address of the owner of the contract
-    address public owner;
-
-    // The mapping of all token balances
     mapping (address => uint256) public balanceOf;
+    mapping (address => mapping (address => uint256)) public allowance;
 
-    // The mapping of all token allowances
-    mapping (address => mapping (address => uint256)) public allowed;
-
-    // The NFT associated with this reward token
-    address public nftContract;
-
-    // The NFT token ID to be awarded when the reward conditions are met
-    uint256 public nftTokenId;
-
-    // The reward conditions
-    uint256 public rewardCondition;
-
-    // The event for when the reward is given
-    event RewardGiven(address recipient, uint256 amount);
-// Constructor
-    constructor(uint256 _totalSupply, address _nftContract, uint256 _nftTokenId, uint256 _rewardCondition) public {
-        // Set the total supply of tokens
-        totalSupply = _totalSupply;
-        // Set the owner of the contract
-        owner = msg.sender;
-        // Set the NFT address
-        nftContract = _nftContract;
-        // Set the NFT token ID
-        nftTokenId = _nftTokenId;
-        // Set the reward condition
-        rewardCondition = _rewardCondition;
-        // Allocate the total supply of tokens to the owner
-        balanceOf[owner] = totalSupply;
+    // constructor
+    constructor() public {
+        name = "TokenA";
+        symbol = "TA";
+        decimals = 18;
+        totalSupply = 100000000000000000;
+        balanceOf[msg.sender] = totalSupply;
     }
 
-    // Transfer tokens from one address to another
-    function transfer(address recipient, uint256 amount) public returns (bool) {
-        require(balanceOf[msg.sender] >= amount, "Insufficient balance.");
-        balanceOf[msg.sender] -= amount;
-        balanceOf[recipient] += amount;
-        emit Transfer(msg.sender, recipient, amount);
+    // transfer
+    function transfer(address _to, uint256 _value) public returns (bool success) {
+        require(balanceOf[msg.sender] >= _value && _value > 0);
+        balanceOf[msg.sender] -= _value;
+        balanceOf[_to] += _value;
+        emit Transfer(msg.sender, _to, _value);
         return true;
     }
 
-    // Function to check if the reward conditions are met and to give the reward if they are
-    function checkReward() public returns (bool) {
-        if (balanceOf[msg.sender] >= rewardCondition) {
-            // Transfer the NFT to the recipient
-            ERC721(_nftContract).transferFrom(owner, msg.sender, nftTokenId);
-
-            // Emit the RewardGiven event
-            emit RewardGiven(msg.sender, nftTokenId);
-
-            return true;
-        }
-        return false;
+    // approve
+    function approve(address _spender, uint256 _value) public returns (bool success) {
+        allowance[msg.sender][_spender] = _value;
+        emit Approval(msg.sender, _spender, _value);
+        return true;
     }
+
+    // transfer from
+    function transferFrom(address _from, address _to, uint256 _value) public returns (bool success) {
+        require(_value <= allowance[_from][msg.sender]);
+        allowance[_from][msg.sender] -= _value;
+        balanceOf[_from] -= _value;
+        balanceOf[_to] += _value;
+        emit Transfer(_from, _to, _value);
+        return true;
+    }
+
+    // airdrop
+    function airdrop(address _to, uint256 _value) public returns (bool success) {
+        require(_value > 0);
+        balanceOf[_to] += _value;
+        emit Transfer(address(0), _to, _value);
+        return true;
+    }
+// redeem
+    function redeem(address _to, uint256 _value) public returns (bool success) {
+        require(_value == 10000);
+        balanceOf[_to] -= _value;
+        emit Redeem(_to, _value);
+        return true;
+    }
+
+    // airdrop nft
+    function airdropNFT(address _to) public returns (bool success) {
+        require(balanceOf[_to] == 10000);
+        balanceOf[_to] = 0;
+        emit AirdropNFT(_to);
+        return true;
+    }
+
+    event Transfer(address indexed _from, address indexed _to, uint256 _value);
+    event Approval(address indexed _owner, address indexed _spender, uint256 _value);
+    event Redeem(address indexed _to, uint256 _value);
+    event AirdropNFT(address indexed _to);
 }
